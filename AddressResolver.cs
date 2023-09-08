7e415367623f23633eecdb4cf72d25a97ef1bd9f
ryo0524 <123471218+ryon5541@users.noreply.github.com>
2023-09-09 01:54:33 +0900

    up

---
+++ AddressResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dalamud.Game;
+
+namespace LeaveDutyCmdPlugin;
+
+internal class AddressResolver : BaseAddressResolver
+{
+    public IntPtr LeaveDuty { get; private set; }
+
+    protected override void Setup64Bit(SigScanner scanner)
+    {
+        LeaveDuty = scanner.ScanText("40 53 48 83 ec 20 48 8b 05 ?? ?? ?? ?? 0f b6 d9");
+    }
+}
---
+++ LeaveDutyCmdPlugin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Dalamud.Game;
+using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Logging;
+using Dalamud.Plugin;
+using static Dalamud.Game.Command.CommandInfo;
+using static Dalamud.Game.Framework;
+
+namespace LeaveDutyCmdPlugin;
+
+internal class LeaveDutyCmdPlugin : IDalamudPlugin, IDisposable
+{
+    private delegate void LeaveDutyDelegate(char is_timeout);
+
+    private const string commandName = "/ql";
+
+    private LeaveDutyDelegate leaveDungeon;
+
+    private readonly AddressResolver AddressResolver;
+
+    private bool requesting = false;
+
+    public string Name => "DutyHop";
+
+    [PluginService]
+    private static DalamudPluginInterface Interface { get; set; }
+
+    [PluginService]
+    private static CommandManager CommandManager { get; set; }
+
+    [PluginService]
+    private static Framework Framework { get; set; }
+
+    public LeaveDutyCmdPlugin()
+    {
+        
+        AddressResolver = new AddressResolver();
+        ((BaseAddressResolver)AddressResolver).Setup();
+        leaveDungeon = Marshal.GetDelegateForFunctionPointer<LeaveDutyDelegate>(AddressResolver.LeaveDuty);
+        Framework.Update += new OnUpdateDelegate(OnFrameworkUpdate);
+        CommandManager.AddHandler("/ql", new CommandInfo(new HandlerDelegate(OnCommand))
+        {
+            HelpMessage = "Immediately leave duty without confirmation window."
+        });
+    }
+
+    public void Dispose()
+    {
+        
+        CommandManager.RemoveHandler("/ql");
+        Framework.Update -= new OnUpdateDelegate(OnFrameworkUpdate);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        requesting = true;
+    }
+
+    private void OnFrameworkUpdate(Framework framework)
+    {
+        try
+        {
+            if (requesting)
+            {
+                leaveDungeon('\0');
+                requesting = false;
+            }
+        }
+        catch (Exception ex)
+        {
+            PluginLog.LogError(ex.Message, Array.Empty<object>());
+        }
+    }
+}
